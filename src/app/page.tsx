2034fdd31b1bf8388fa22d5d3437f8ddfcbe8db4
井上頌英 <inoueshouei@fucck-mini.local>
2025-03-23 23:27:39 +0900

    無理やりglobal.cssのカラーを独自の色に変更した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Button, buttonVariants } from "@/components/ui/button";
+import { Secondary } from "@/stories/Button.stories";
 import Image from "next/image";
+import { describe } from "vitest";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      TOPページ
-      <Button>テスト</Button>
-    </div>
-  );
+		<div>
+			<Button>Primary</Button>
+			<Button variant={"outline"}>Secondaly</Button>
+			<Button variant={"destructive"}>Black</Button>
+		</div>
+	);
 }
