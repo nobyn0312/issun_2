875f69291bbc99cc1511d98f99e2fa4f37f9dac7
井上頌英 <inoueshouei@fucck-mini.local>
2025-03-30 11:22:57 +0900

    カード

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,63 +4,76 @@ import Image from "next/image";
 import { describe } from "vitest";
 import { cn, getCustomColor } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import { Container } from "@/components/features/container";
+import { Card } from "@/components/features/card";
 
 export default function Home() {
 	return (
-		<div className='space-y-5'>
-			<p>ボタンコンポーネント</p>
-			<Button
-				className={cn(
-					"min-w-[220px] max-w-[220px] md:max-w-[280px] hover:bg-[#ffa789]",
-					getCustomColor("brand-primary")
-				)}
-				style={{ display: "block" }}
-			>
-				Primary Button
-			</Button>
+		<>
+			<Container>
+				<div className='space-y-5'>
+					<p>ボタンコンポーネント</p>
+					<Button
+						className={cn(
+							"min-w-[220px] max-w-[220px] md:max-w-[280px] hover:bg-[#ffa789]",
+							getCustomColor("brand-primary")
+						)}
+						style={{ display: "block" }}
+					>
+						Primary Button
+					</Button>
 
-			<Button
-				className={cn(
-					"min-w-[220px] max-w-[220px] md:max-w-[280px] hover:bg-[#6b6b6b]",
-					getCustomColor("brand-secondary")
-				)}
-				style={{ display: "block" }}
-			>
-				Secondary Button
-			</Button>
+					<Button
+						className={cn(
+							"min-w-[220px] max-w-[220px] md:max-w-[280px] hover:bg-[#6b6b6b]",
+							getCustomColor("brand-secondary")
+						)}
+						style={{ display: "block" }}
+					>
+						Secondary Button
+					</Button>
 
-			<Button
-				className={cn(
-					"min-w-[220px] max-w-[220px] md:max-w-[280px] hover:bg-[#ffffff7b] hover:opacity-60",
-					getCustomColor("brand-outline")
-				)}
-				style={{ display: "block" }}
-			>
-				Outline Button
-			</Button>
+					<Button
+						className={cn(
+							"min-w-[220px] max-w-[220px] md:max-w-[280px] hover:bg-[#ffffff7b] hover:opacity-60",
+							getCustomColor("brand-outline")
+						)}
+						style={{ display: "block" }}
+					>
+						Outline Button
+					</Button>
+				</div>
 
-			<p>Input</p>
-			<Input type='email' placeholder='Email' className='w-[220px]' />
+				<p>Input</p>
+				<Input type='email' placeholder='Email' className='w-[220px]' />
+			</Container>
+			<p>Card</p>
 
-			<p>
-				shadcn--------------------------------------------------------------------
-			</p>
-			<div className='font-bold'>Variant</div>
-			<div className='flex space-x-5'>
-				<Button>Default</Button>
-				<Button variant='destructive'>Destructive</Button>
-				<Button variant='outline'>Outline</Button>
-				<Button variant='secondary'>Secondary</Button>
-				<Button variant='ghost'>Ghost</Button>
-				<Button variant='link'>Link</Button>
+			<div style={{ display:"flex"}}>
+				<Card />
+				<Card />
+				<Card />
 			</div>
-			<div className='font-bold'>Size</div>
-			<div className='flex space-x-5'>
-				<Button>Default</Button>
-				<Button size='sm'>Small</Button>
-				<Button size='lg'>Large</Button>
-				<Button size='icon'>Icon</Button>
-			</div>
-		</div>
+
+			{/* <Container>
+				<p>shadcn------------------------------------------</p>
+				<div className='font-bold'>Variant</div>
+				<div className='flex space-x-5'>
+					<Button>Default</Button>
+					<Button variant='destructive'>Destructive</Button>
+					<Button variant='outline'>Outline</Button>
+					<Button variant='secondary'>Secondary</Button>
+					<Button variant='ghost'>Ghost</Button>
+					<Button variant='link'>Link</Button>
+				</div>
+				<div className='font-bold'>Size</div>
+				<div className='flex space-x-5'>
+					<Button>Default</Button>
+					<Button size='sm'>Small</Button>
+					<Button size='lg'>Large</Button>
+					<Button size='icon'>Icon</Button>
+				</div>
+			</Container> */}
+		</>
 	);
 }
--- src/components/features/card.tsx
+++ src/components/features/card.tsx
@@ -1,16 +1,18 @@
-// components/ui/card.tsx
-import { cn } from "@/lib/utils";
+import React from "react";
+import Image from "next/image";
 
-export function Card({
-	className,
-	children,
-}: {
-	className?: string;
-	children: React.ReactNode;
-}) {
+export const Card = () => {
 	return (
-		<div className={cn("rounded-lg border bg-white p-4 shadow", className)}>
-			{children}
+		<div>
+			<Image
+				src='/item_sample.png' // 画像のパス（publicフォルダ内の場合）
+				alt='説明文' // アクセシビリティのための代替テキスト
+				width={130} // 画像の幅（ピクセル）
+				height={160} // 画像の高さ（ピクセル）
+			/>
+			<p style={{ fontSize: "10px", fontWeight: "bold" }}>BRAND</p>
+			<p style={{ fontSize: "10px", fontWeight: "normal" }}>category</p>
+			<p style={{ fontSize: "14px", fontWeight: "bold" ,color: "#FF5E2A"}}>￥1,980</p>
 		</div>
 	);
-}
+};
