c7314abb86c5d24ffe0e9f6e8dc76662a53d103e
inoue-nobuhide <inoue-nobuhide@users.noreply.github.com>
2025-04-01 19:51:54 +0900

    tsconfigの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{js,jsx,ts,tsx}", // ItemCard.tsx がここにある
-	],
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			colors: {
-				"primary-orange": "#FF5E2A",
+				primary: {
+					orange: "#FF5E2A",
+				},
 			},
 		},
 	},
