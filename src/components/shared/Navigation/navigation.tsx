ab6193ad4073577e65ca0bb2c493da703b665b5e
inoue-nobuhide <inoue-nobuhide@users.noreply.github.com>
2025-04-03 18:51:30 +0900

    navigatin追加

---
+++ src/components/shared/Navigation/navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Navigation = () => {
+  return (
+    <div>
+      <ul className={"flex gap-3 "}>
+        <li>HOME</li>
+        <li>Favorite</li>
+        <li>Search</li>
+        <li>Additem</li>
+        <li>Mypage</li>
+      </ul>
+    </div>
+  )
+}
