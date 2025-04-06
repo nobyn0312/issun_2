1d35f088180b006f4edbb15eeeb9f86e8c990ea3
井上頌英 <inoueshouei@fucck-mini.local>
2025-04-06 14:45:14 +0900

    itemCardのスタイルをtailwindに修正

--- src/components/shared/ItemCard/item-card.tsx
+++ src/components/shared/ItemCard/item-card.tsx
@@ -1,38 +1,41 @@
-	import React from "react";
-	import Image from "next/image";
-	import { StaticImageData } from "next/image";
-	import styles from "./itemcard.module.css";
+import React from "react";
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+import styles from "./itemcard.module.css";
 import Link from "next/link";
-	interface ItemCardProps {
-		imageSrc: string;
-		brand: string;
-		category: string;
-		price: number;
-	}
-	export const ItemCard = ({
-		imageSrc,
-		brand,
-		category,
-		price,
-	}: ItemCardProps) => {
-		const priceInt = price.toLocaleString();
-		return (
-			<Link href={"#"} className={styles.itemCard}>
-				<div>
-					<div className={styles.item}>
-						<Image
-							src={imageSrc}
-							alt='item'
-							fill
-							sizes='(max-width: 768px) 33vw' // 画面幅の1/3
-						/>
-					</div>
-					<div className='px-1 py-2'>
-						<p className='text-xs font-bold'>{brand}</p>
-						<p className='text-xs'>{category}</p>
-						<p className='text-sm font-bold text-orange-500'>{priceInt}</p>
-					</div>
+interface ItemCardProps {
+	imageSrc: string;
+	brand: string;
+	category: string;
+	price: number;
+}
+export const ItemCard = ({
+	imageSrc,
+	brand,
+	category,
+	price,
+}: ItemCardProps) => {
+	const priceInt = price.toLocaleString();
+	return (
+		<Link
+			href={"#"}
+			className='flex flex-col grow shrink-0 max-w-64 basis-[30vw] w-full'>
+			<div>
+				<div
+					className='relative w-full aspect-[1/1.3]'>
+					<Image
+						src={imageSrc}
+						alt='item'
+						fill
+						sizes='(max-width: 768px) 33vw'
+					/>
 				</div>
-			</Link>
-		);
-	};
+				<div className='px-1 py-2'>
+					<p className='text-xs font-bold'>{brand}</p>
+					<p className='text-xs'>{category}</p>
+					<p className='text-sm font-bold text-orange-500'>{priceInt}</p>
+				</div>
+			</div>
+		</Link>
+	);
+};
