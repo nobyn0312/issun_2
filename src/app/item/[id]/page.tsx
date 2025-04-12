09d3fa81c588b8cd5ee93159f26e390b1bda8c3a
井上頌英 <inoueshouei@fucck-mini.local>
2025-04-12 18:19:10 +0900

    白色のcontentswrapperを追加

--- src/app/item/[id]/page.tsx
+++ src/app/item/[id]/page.tsx
@@ -17,18 +17,19 @@ const detail = () => {
 					className='object-cover mx-auto block'
 				/>
 
-				<ContentsWrapper variant='orange'>
+				<ContentsWrapper variant='orange' className='mb-4'>
+				<p className='text-sm font-bold'>BRAND: Uniqlo</p>
 				<h2 className='text-xl font-bold'>
 						エアリズムオーバーサイズコットンTシャツ
 					</h2>
 					<div className='mt-2'>
 						<p className='text-sm font-semibold'>PRICE</p>
-						<p className='text-lg'>¥1,980</p>
+						<p className='text-lg font-bold'>¥1,980</p>
 					</div>
 					<div className='mt-2'>
 						<p className='text-sm font-semibold'>DETAIL</p>
 						<p className='text-sm'>
-							詳細 フォルムと着心地を極めた1枚。 Uniqlo U 仕様 -
+							フォルムと着心地を極めた1枚。 Uniqlo U 仕様 -
 							コットンの見た目でサラリと快適な「エアリズム」。-
 							やや首元が詰まったクルーネックでクリーンな印象。-
 							ドロップショルダーでゆったりとしたフィットの5袖。-
@@ -37,70 +38,42 @@ const detail = () => {
 						</div>
 				</ContentsWrapper>
 
-				<ContentsWrapper variant='black'>
-					<div className='mt-2'>
-						<p className='text-sm font-semibold'>DETAIL</p>
-						<p className='text-sm'>
-							詳細 フォルムと着心地を極めた1枚。 Uniqlo U 仕様 -
-							コットンの見た目でサラリと快適な「エアリズム」。-
-							やや首元が詰まったクルーネックでクリーンな印象。-
-							ドロップショルダーでゆったりとしたフィットの5袖。-
-							シルエットがきれいに出る素材感。
-						</p>
-						</div>
-				</ContentsWrapper>
-
-				<ContentsWrapper variant='default'>
-					<div className='mt-2'>
-						<p className='text-sm font-semibold'>DETAIL</p>
-						<p className='text-sm'>
-							詳細 フォルムと着心地を極めた1枚。 Uniqlo U 仕様 -
-							コットンの見た目でサラリと快適な「エアリズム」。-
-							やや首元が詰まったクルーネックでクリーンな印象。-
-							ドロップショルダーでゆったりとしたフィットの5袖。-
-							シルエットがきれいに出る素材感。
-						</p>
-						</div>
-				</ContentsWrapper>
-
-				<div className='mt-4 bg-orange-500 px-4 py-4 text-white rounded-md'>
-					<p className='text-sm text-gray-500'>BRAND: Uniqlo</p>
-					<h2 className='text-xl font-bold'>
-						エアリズムオーバーサイズコットンTシャツ
-					</h2>
-					<div className='mt-2'>
-						<p className='text-sm font-semibold'>PRICE</p>
-						<p className='text-lg'>¥1,980</p>
-					</div>
-					<div className='mt-2'>
-						<p className='text-sm font-semibold'>DETAIL</p>
-						<p className='text-sm'>
-							詳細 フォルムと着心地を極めた1枚。 Uniqlo U 仕様 -
-							コットンの見た目でサラリと快適な「エアリズム」。-
-							やや首元が詰まったクルーネックでクリーンな印象。-
-							ドロップショルダーでゆったりとしたフィットの5袖。-
-							シルエットがきれいに出る素材感。
-						</p>
-					</div>
-				</div>
-
-				<div className='mt-4 px-4 py-4 bg-gray-200 rounded-md'>
-					<p className='text-sm text-gray-500'>Minnano</p>
+				<ContentsWrapper>
+				<p className='text-sm text-gray-500'>Minnano</p>
 					<p className='text-sm font-semibold'>Review</p>
 					<p className='text-sm'>reviewer：32</p>
+
+					<ContentsWrapper variant='white' className={"mb-2"}>
 					<div className='mt-2'>
 						<div className='text-sm font-medium'>着丈ちょうどいい</div>
 						<p className='text-xs text-gray-500'>2024/09/09</p>
 						<p className='text-sm'>★★★★☆</p>
-						<p className='text-sm'>SIZE：S</p>
+						<p className='text-sm'>SIZE：M</p>
 						<p className='text-sm'>
 							白のMサイズを購入
 							大きめで着るのが好きなので袖も長めでちょうどいいです
 							着丈も長めですがいつもインして着ています
 							洗濯してもシワになりづらく、お気に入りです
 						</p>
+						<p>一寸法師/男性/160cm/やや細身</p>
 					</div>
-				</div>
+					</ContentsWrapper>
+
+					<ContentsWrapper variant='white'>
+					<div className='mt-2'>
+						<div className='text-sm font-medium'>小さくても似合う神Tシャツ</div>
+						<p className='text-xs text-gray-500'>2024/09/09</p>
+						<p className='text-sm'>★★★★☆</p>
+						<p className='text-sm'>SIZE：M</p>
+						<p className='text-sm'>
+						良いです。
+						</p>
+
+						<p>チョロ松/男性/165cm/やや細身</p>
+					</div>
+					</ContentsWrapper>
+				</ContentsWrapper>
+
 
 				<div className='mt-6 flex flex-col items-center'>
 					<p className='text-sm text-gray-500'>BRAND: Uniqlo</p>
