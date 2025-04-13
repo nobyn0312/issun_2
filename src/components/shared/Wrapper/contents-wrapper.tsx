9b0148dfd951dd71f2fb141b92e4f629d0dd5618
井上頌英 <inoueshouei@fucck-mini.local>
2025-04-13 19:52:48 +0900

    content-wrapper dropshadow

--- src/components/shared/Wrapper/contents-wrapper.tsx
+++ src/components/shared/Wrapper/contents-wrapper.tsx
@@ -11,9 +11,14 @@ const contentsWrapperVariants = cva("rounded-lg py-4", {
 			black: "bg-black text-white",
 			white: "bg-white text-black",
 		},
+		shadow: {
+      none: "",
+      md: "shadow-md",
+    },
 	},
 	defaultVariants: {
 		variant: "default",
+		shadow: "md",
 	},
 });
 
@@ -24,11 +29,12 @@ export const ContentsWrapper = ({
 	children,
 	className,
 	variant,
+	shadow,
 	...props
 }: Props) => {
 	return (
 		<div
-			className={cn(contentsWrapperVariants({ variant }), className)}
+			className={cn(contentsWrapperVariants({ variant,shadow }), className)}
 			{...props}
 		>
 			<Container>{children}</Container>
