85b7796d95f765decf6c80be228f2dd4100f8070
nobyn0312 <nobyn0312@gmail.com>
2025-04-14 09:42:32 +0900

    サインアップ画面、layout修正

--- src/app/auth/signUp/layout.tsx
+++ src/app/auth/signUp/layout.tsx
@@ -2,23 +2,20 @@ import { Container } from "@/components/features/container";
 import { ContentsWrapper } from "@/components/shared/Wrapper/contents-wrapper";
 import Link from "next/link";
 
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-        <Container>
-      <header className="mb-8">
-        <h1 className="text-xl font-bold">ISSUN</h1>
-      </header>
-
-      <ContentsWrapper variant="orange">
-      <main className="p-8 rounded-lg w-full max-w-md">
-        {children}
-      </main>
-      </ContentsWrapper>
-
-
-    </Container>
-    </>
-
-  );
+export default function AuthLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<>
+			<Container>
+				<header className='mb-8'>
+					<h1 className='text-xl font-bold'>ISSUN</h1>
+				</header>
+				<div className='max-w-md mx-auto'>{children}
+				</div>
+			</Container>
+		</>
+	);
 }
--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -1,27 +1,31 @@
+import { Container } from "@/components/features/container";
 import { ContentsWrapper } from "@/components/shared/Wrapper/contents-wrapper";
 import { Button } from "@/components/ui/Button/button";
 import { TextField } from "@/components/ui/TextField/text-field";
+import Link from "next/link";
 
 export default function SignUpPage() {
-  return (
-    <div>
-
-      <p className='text-sm font-bold'>新規登録</p>
-     <h2 className='text-xl font-bold mb-3'>SignUp</h2>
-      <form>
-        <input type="email" placeholder="メールアドレス" className="border p-2 w-full mb-4" />
-        <input type="password" placeholder="パスワード" className="border p-2 w-full mb-4" />
-
-        <TextField type="name" placeholder="名前"></TextField>
-        {/* <button type="submit" className="bg-blue-500 text-white p-2 w-full rounded">
-          登録
-        </button> */}
-        <Button variant="secondary">
-          新規登録
-        </Button>
-      </form>
-
-
-    </div>
-  );
+	return (
+		<>
+			<ContentsWrapper variant='orange'>
+				<p className='text-sm font-bold'>新規登録</p>
+				<h2 className='text-xl font-bold mb-3'>SignUp</h2>
+				<form>
+					<TextField type='name' placeholder='名前'></TextField>
+					<div className='inner mb-4'>
+						<TextField type='email' placeholder='メール'></TextField>
+						<TextField type='password' placeholder='パスワード'></TextField>
+					</div>
+					<div className='flex align-middle justify-center'>
+						<Button variant='secondary'>新規登録</Button>
+					</div>
+				</form>
+			</ContentsWrapper>
+			<div>
+				<Link href='#'>
+					<p className='text-center'>すでにアカウントお持ちの方はこちら</p>
+				</Link>
+			</div>
+		</>
+	);
 }
