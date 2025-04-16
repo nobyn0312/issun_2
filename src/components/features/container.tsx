a37a0e0865181fd55fd32eebe807dce80ab8a42c
井上頌英 <inoueshouei@fucck-mini.local>
2025-04-16 23:21:02 +0900

    containerコンポーネント修正

--- src/components/features/container.tsx
+++ src/components/features/container.tsx
@@ -1,16 +1,20 @@
 import { ReactNode } from "react";
 
 type Props = {
-  children: ReactNode;
-  className?: string; // classNameを追加
+	children: ReactNode;
+	className?: string; // classNameを追加
 };
 
 export const Container = ({ children, className = "" }: Props) => {
-  const style = {
-    maxWidth: "768px",
-    margin: "0 auto",
-    padding: "0 16px",
-  };
+	const style = {
+		maxWidth: "800px",
+		margin: "0 auto",
+		padding: "0 16px",
+	};
 
-  return <div style={style} className={className}>{children}</div>;
+	return (
+		<div style={style} className={className}>
+			{children}
+		</div>
+	);
 };
