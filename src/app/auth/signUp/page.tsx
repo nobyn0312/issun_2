105e29edb63be0780fe6081938b4a273e544a6c2
inoue-nobuhide <inoue-nobuhide@users.noreply.github.com>
2025-04-17 18:47:13 +0900

    auth/signUpページ修正

--- src/app/auth/signUp/page.tsx
+++ src/app/auth/signUp/page.tsx
@@ -7,13 +7,21 @@ import Link from "next/link";
 export default function SignUpPage() {
 	return (
 		<>
-			<ContentsWrapper variant='orange'>
+			<ContentsWrapper variant='orange' className='mb-4'>
 				<p className='text-sm font-bold'>新規登録</p>
 				<h2 className='text-xl font-bold mb-3'>SignUp</h2>
 				<form>
-					<TextField type='name' placeholder='名前'></TextField>
-					<div className='inner mb-4'>
-						<TextField type='email' placeholder='メール'></TextField>
+					<TextField
+						type='name'
+						placeholder='名前'
+						className='mb-2'
+					></TextField>
+					<div className='inner mb-8'>
+						<TextField
+							type='email'
+							placeholder='メール'
+							className='mb-2'
+						></TextField>
 						<TextField type='password' placeholder='パスワード'></TextField>
 					</div>
 					<div className='flex align-middle justify-center'>
@@ -22,8 +30,10 @@ export default function SignUpPage() {
 				</form>
 			</ContentsWrapper>
 			<div>
-				<Link href='#'>
-					<p className='text-center'>すでにアカウントお持ちの方はこちら</p>
+				<Link
+					href='/auth/login'
+					className='block border-b pb-[2px] text-sm text-center mx-auto my-0 w-fit'>
+					すでにアカウントお持ちの方はこちら
 				</Link>
 			</div>
 		</>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,39 @@
 "use client";
 
 import { Button, buttonVariants } from "@/components/ui/Button/button";
-import { TextField } from "@/components/ui/TextField/text-field";
 import { Container } from "@/components/features/container";
 import { ItemCard } from "@/components/shared/ItemCard/item-card";
-import { Header } from "@/components/shared/Header/header";
 import { Navigation } from "@/components/shared/Navigation/navigation";
-import { Textarea } from "@/components/ui/TextArea/textarea";
+import Image from "next/image";
+import topLogo from "/public//images/topLogo.svg";
+import Link from "next/link";
+
 export default function Home() {
 	return (
 		<>
-			<Header />
+			{/* <div className={"py-4"}>
+				<Container>
+					<Image
+						src={topLogo}
+						alt={"ロゴ"}
+						width={768}
+						height={768}
+						style={{ display: "block", margin: "0 auto" }}
+					/>
+				</Container>
+			</div> */}
 			<Container>
-				<div className='flex flex-wrap justify-center'>
+				<div className='flex flex-wrap justify-center py-4 mb-10'>
 					{[...Array(9)].map((_, i) => (
-						<ItemCard
+						<Link href="#">
+												<ItemCard
 							imageSrc='/images/item_sample.png'
 							brand='UNIQLO'
 							category='tops'
 							price={1980}
 							key={i}
 						/>
+						</Link>
 					))}
 				</div>
 			</Container>
