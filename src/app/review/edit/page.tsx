1a369870511570adb3c6e583a67dd6e9bfca4006
inoue-nobuhide <inoue-nobuhide@users.noreply.github.com>
2025-04-18 17:39:26 +0900

    review add edit 追加

---
+++ src/app/review/edit/page.tsx
@@ -0,0 +1,17 @@
+import { Container } from '@/components/features/container'
+import { ContentsWrapper } from '@/components/shared/Wrapper/contents-wrapper'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Container>
+      <ContentsWrapper variant="default">
+        <h1>Edit</h1>
+      </ContentsWrapper>
+    </Container>
+    </>
+  )
+}
+
+export default page
