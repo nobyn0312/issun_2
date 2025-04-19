00d55f2ea9fad6c28d807380f858518d5fe549df
nobyn0312 <nobyn0312@gmail.com>
2025-04-19 18:25:06 +0900

    slect追加

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,36 +1,18 @@
-import { Container } from "@/components/features/container";
-import { ContentsWrapper } from "@/components/shared/Wrapper/contents-wrapper";
-import { Button } from "@/components/ui/Button/button";
-import { TextField } from "@/components/ui/TextField/text-field";
-import Link from "next/link";
+import { Container } from '@/components/features/container'
+import { ContentsWrapper } from '@/components/shared/Wrapper/contents-wrapper'
+import React from 'react'
 
-export default function SignUpPage() {
-	return (
-		<>
-			<ContentsWrapper variant='orange' className='mb-4'>
-				<p className='text-sm font-bold'>ログイン</p>
-				<h2 className='text-xl font-bold mb-3'>Login</h2>
-				<form>
-					<div className='inner mb-8'>
-						<TextField
-							type='email'
-							placeholder='メール'
-							className='mb-2'
-						></TextField>
-						<TextField type='password' placeholder='パスワード'></TextField>
-					</div>
-					<div className='flex align-middle justify-center'>
-						<Button variant='secondary'>ログイン</Button>
-					</div>
-				</form>
-			</ContentsWrapper>
-			<div>
-				<Link
-					href='/auth/signUp'
-					className='block border-b pb-[2px] text-sm text-center mx-auto my-0 w-fit'>
-					まだアカウントお持ちでない方
-				</Link>
-			</div>
-		</>
-	);
+const page = () => {
+  return (
+    <>
+    <Container>
+      <ContentsWrapper variant="default">
+      <p className='text-sm font-bold'>Review Edit</p>
+      <h2 className='text-xl font-bold mb-3'>レビュー編集</h2>
+      </ContentsWrapper>
+    </Container>
+    </>
+  )
 }
+
+export default page
--- src/app/review/add/page.tsx
+++ src/app/review/add/page.tsx
@@ -1,17 +1,81 @@
-import { Container } from '@/components/features/container'
-import { ContentsWrapper } from '@/components/shared/Wrapper/contents-wrapper'
-import React from 'react'
+import { Container } from "@/components/features/container";
+import { ContentsWrapper } from "@/components/shared/Wrapper/contents-wrapper";
+import { Button } from "@/components/ui/Button/button";
+import { TextField } from "@/components/ui/TextField/text-field";
+import { Textarea } from "../../../components/ui/TextArea/textarea";
+
+import React from "react";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
 const page = () => {
-  return (
-    <>
-    <Container>
-      <ContentsWrapper variant="default">
-        <h1>Review</h1>
-      </ContentsWrapper>
-    </Container>
-    </>
-  )
-}
+	return (
+		<>
+			<Container>
+				<ContentsWrapper variant='default'>
+					<p className='text-sm font-bold'>Review</p>
+					<h2 className='text-xl font-bold mb-3'>レビュー投稿</h2>
 
-export default page
+					<form>
+						<p>タイトル</p>
+						<TextField
+							type='name'
+							placeholder='名前'
+							className='mb-2'
+						></TextField>
+						<div className='inner mb-8'>
+							<p>購入サイズ</p>
+							<Select>
+								<SelectTrigger className='w-[180px]'>
+									<SelectValue placeholder='選択してください' />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										<SelectLabel>size</SelectLabel>
+										<SelectItem value='XS'>XS</SelectItem>
+										<SelectItem value='S'>S</SelectItem>
+										<SelectItem value='M'>M</SelectItem>
+										<SelectItem value='L'>L</SelectItem>
+										<SelectItem value='XL'>XL</SelectItem>
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+
+							<p>評価</p>
+							<Select>
+								<SelectTrigger className='w-[180px]'>
+									<SelectValue placeholder='選択してください' />
+								</SelectTrigger>
+								<SelectContent>
+									<SelectGroup>
+										<SelectLabel>rate</SelectLabel>
+										<SelectItem value='1'>★☆☆☆☆</SelectItem>
+										<SelectItem value='2'>★★☆☆☆</SelectItem>
+										<SelectItem value='3'>★★★☆☆</SelectItem>
+										<SelectItem value='4'>★★★★☆</SelectItem>
+										<SelectItem value='5'>★★★★★</SelectItem>
+									</SelectGroup>
+								</SelectContent>
+							</Select>
+
+							<p>コメント</p>
+							<Textarea></Textarea>
+						</div>
+						<div className='flex align-middle justify-center'>
+							<Button variant='secondary'>レビュー投稿</Button>
+						</div>
+					</form>
+				</ContentsWrapper>
+			</Container>
+		</>
+	);
+};
+
+export default page;
