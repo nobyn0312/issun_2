7fc42e0501086961df11270139dc055ab7b754b3
井上頌英 <inoueshouei@fuck-mini.local>
2025-05-11 23:34:13 +0900

    reacthookform zod

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -5,11 +5,45 @@ import { ContentsWrapper } from "@/components/shared/Wrapper/contents-wrapper";
 import { Button } from "@/components/ui/Button/button";
 import { TextField } from "@/components/ui/TextField/text-field";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { StringValidation } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { validationSchema } from "@/app/utils/validationScheme";
 
-export default function SignUpPage() {
+interface signInForm {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default function SignInPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signInForm>({
+    mode: "onChange",
+    resolver: zodResolver(validationSchema),
+  });
+
+  const onsubmit = (data: signInForm) => {
+    console.log(data);
+  };
+
   return (
     <>
+
+<form onSubmit={handleSubmit(onsubmit)}>
+          <label htmlFor="名前">名前</label>
+          <input id="name" type="text" {...register("name")} />
+          <p>{errors.name?.message as React.ReactNode}</p>
+          <input type="text" />
+
+          <button type='submit'>送信</button>
+        </form>
+
       <ContentsWrapper variant="orange" className="mb-4">
+
         <p className="text-sm font-bold">サインイン</p>
         <h2 className="text-xl font-bold mb-3">signIn</h2>
         <form>
